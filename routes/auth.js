68f3a9c00134c867102b027e51caff23f09c2a6e
Eloi-Perez <eloi104@hotmail.com>
2022-08-17 14:15:09 +0100

    feat: create passport integration

---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const passportJWT = require('passport-jwt')
+
+const Models = require('./models.js')
+const Users = Models.User
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
+passport.use(new LocalStrategy({
+	usernameField: 'Username',
+	passwordField: 'Password'
+}, (username, password, callback) => {
+	console.log('Logging in: ' + username);
+	Users.findOne({ Username: username }, (error, user) => {
+		if (error) {
+			console.log(error)
+			return callback(error)
+		}
+
+		if (!user) {
+			console.log('incorrect username')
+			return callback(null, false, { message: 'Incorrect username.' })
+		}
+
+		if (!user.validatePassword(password)) {
+			console.log('incorrect password')
+			return callback(null, false, { message: 'Incorrect password.' })
+		}
+
+		console.log('finished login')
+		return callback(null, user)
+	})
+}))
+
+passport.use(new JWTStrategy({
+	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+	secretOrKey: process.env.JWT_KEY
+}, (jwtPayload, callback) => {
+	return Users.findById(jwtPayload._id)
+		.then((user) => {
+			return callback(null, user)
+		})
+		.catch((error) => {
+			return callback(error)
+		})
+}))
