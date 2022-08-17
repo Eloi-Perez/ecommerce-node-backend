cc6de8e1f59885a6a773797d1858470e110a57d1
Eloi-Perez <eloi104@hotmail.com>
2022-08-17 10:37:31 +0100

    feat: add json parsing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const app = express()
+app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT || 8030
