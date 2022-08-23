3721fe6d58551b2ced553ec56deeda0d5430efa6
Eloi-Perez <eloi104@hotmail.com>
2022-08-23 19:09:12 +0100

    feat: add localAuth to routes

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ const router = express.Router()
 router.post('/signup', cont.registerUser)
 router.post('/login', midd.localAuth, cont.loginUser)
 router.post('/logout', cont.logoutUser)
-router.put('/update', cont.updateUser)
-router.put('/disable', cont.disableUser)
+router.put('/update', midd.localAuth, cont.updateUser)
+router.put('/disable', midd.localAuth, cont.disableUser)
 router.delete('/delete', cont.deleteUser) //only admin
 
 module.exports = router
