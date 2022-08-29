efc3e33897859b262e6ebb2081042d10990381b4
Begli Kakabalov <begli.kaka@mail.ru>
2022-08-29 08:56:23 +0200

    feat.add validation

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const { localAuth, jwtAdminAuth } = require('../middlewares/middlewares')
 const cont = require('../controllers/users')
+const { body } = require('express-validator')
+
+const { userValidate } = require('../validations/validation')
 
 const router = express.Router()
 
-router.post('/signup', cont.registerUser)
-router.post('/login', localAuth, cont.loginUser)
+router.post('/signup', userValidate, cont.registerUser)
+router.post('/login', userValidate, localAuth, cont.loginUser)
 // router.post('/logout', cont.logoutUser)
 router.put('/update', localAuth, cont.updateUser)
 router.put('/disable', localAuth, cont.disableUser)
---
+++ validations/validation.js
@@ -0,0 +1,31 @@
+const { body } = require('express-validator')
+
+const userValidate = [
+  body('name')
+    .trim()
+    .exists()
+    .withMessage('Name is required')
+    .isLength({ min: 4 })
+    .withMessage('Name should be at least 4 characters'),
+  body('surname')
+    .trim()
+    .exists()
+    .withMessage('Surname is required')
+    .isLength({ min: 4 })
+    .withMessage('Surname should be at least 4 characters'),
+  body('password')
+    .trim()
+    .exists()
+    .withMessage('Password is required')
+    .isLength({ min: 5, max: 10 })
+    .withMessage('Password should be at least 5 characters'),
+  body('email')
+    .exists()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Provide valid email')
+    .normalizeEmail()
+    .trim()
+]
+
+module.exports = { userValidate }
