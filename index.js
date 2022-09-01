83df8cccd51c8b488106d8afd62ca667b3c5800a
Eloi-Perez <eloi104@hotmail.com>
2022-09-01 09:44:27 +0100

    feat: 404 error & organize code

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require('passport')
-const app = express()
 
 const usersRouter = require('./routes/users')
 const productsRouter = require('./routes/products')
 
+const app = express()
+
 mongoose
   .connect(process.env.MONGODB_URI, {})
-  .then(() => {
-    console.log('MongoDB connection is successful')
-  })
-  .catch(() => {
-    console.log('MongoDB connection is failed')
-  })
+  .then(() => { console.log('MongoDB connection is successful') })
+  .catch(() => { console.log('MongoDB connection is failed') })
 
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors()) // TODO options for production
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 
 app.use('/users', usersRouter)
 app.use('/products', productsRouter)
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: '404, endpoint not found' })
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
