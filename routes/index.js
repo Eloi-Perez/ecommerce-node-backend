b4321a0d67591039513fee4fd3bd34d398e09e11
Eloi-Perez <eloi104@hotmail.com>
2023-02-24 22:30:16 +0000

    fix: file import

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const productsRoutes = require('./products')
 const usersRoutes = require('./users')
-const emailRoutes = require('./users')
+const emailRoutes = require('./email')
 
 router.use('/products', productsRoutes)
 router.use('/users', usersRoutes)
